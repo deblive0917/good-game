ec6d9a975bc921f817392c7961f7b0c64537d713
debdebbb <pda840917@gmail.com>
2020-12-02 19:36:58 +0800

    Initial Commit

---
+++ good gameTests/good_gameTests.swift
@@ -0,0 +1,33 @@
+//
+//  good_gameTests.swift
+//  good gameTests
+//
+//  Created by debbb on 2020/12/2.
+//
+
+import XCTest
+@testable import good_game
+
+class good_gameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
